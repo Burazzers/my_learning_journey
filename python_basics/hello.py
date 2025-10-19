0540ad558b09782bd8b1d5c444235b3a5835271d
Burazzers <pavlejevtic@gmail.com>
2025-10-19 21:05:08 +0200

    First python line

---
+++ python_basics/hello.py
@@ -0,0 +1,7 @@
+# hello.py
+print("Zdravo Pavle! Dobrodošao u svet Pythona 🎉")
+
+ime = input("Kako se zoveš? ")
+godine = int(input("Koliko imaš godina? "))
+
+print(f"Lepo je upoznati te, {ime}! Ti imaš {godine} godina.")
